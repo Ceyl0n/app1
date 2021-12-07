0d8c29088285297471752a78416c58d0b19c3127
Andrey Getmanov <ceyl0n@yandex.ru>
2021-12-07 15:14:55 +0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import QW from "@/global/common/qw";
+import QW2 from "@/global/common/qw2";
+
 export default function Home() {
   return (
     <main>
       <h1>Hellow world</h1>
+      <QW />
+      <QW2 />
     </main>
   );
 }
